83130d5c4c08dd7fedd61246031ef5af02037ee8
ala.gr8@code.google.com <ala.gr8@wp.pl>
2015-10-25 22:04:09 +0100

    kom

---
+++ Crypto_L2_1/src/Xor_Hex.java
@@ -0,0 +1,34 @@
+/*
+ * To nie jest moj kod - wykorzystuje go tylko do xorowania dwoch stringow w formacie hex
+ * Zrodlo: http://stackoverflow.com/questions/9840675/xor-hex-string-in-java-of-different-length
+ */
+public class Xor_Hex {
+public String xorHex(String a, String b) {
+    // TODO: Validation
+    char[] chars = new char[a.length()];
+    for (int i = 0; i < chars.length; i++) {
+        chars[i] = toHex(fromHex(a.charAt(i)) ^ fromHex(b.charAt(i)));
+    }
+    return new String(chars);
+}
+
+private static int fromHex(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+    if (c >= 'A' && c <= 'F') {
+        return c - 'A' + 10;
+    }
+    if (c >= 'a' && c <= 'f') {
+        return c - 'a' + 10;
+    }
+    throw new IllegalArgumentException();
+}
+
+private char toHex(int nybble) {
+    if (nybble < 0 || nybble > 15) {
+        throw new IllegalArgumentException();
+    }
+    return "0123456789ABCDEF".charAt(nybble);
+}
+}
